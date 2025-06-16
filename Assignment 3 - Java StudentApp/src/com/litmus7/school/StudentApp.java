c04914215ea0c5d18c411db7d58ae0548215fb4b
Muhammed Irfan <malakkalirfan@gmail.com>
2025-06-16 20:13:01 +0530

    formatting changes

--- Assignment 3 - Java StudentApp/src/com/litmus7/school/StudentApp.java
+++ Assignment 3 - Java StudentApp/src/com/litmus7/school/StudentApp.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
  * <p>
  * This class is responsible for:
  * <ul>
- *   <li>Accepting the number of students</li>
- *   <li>Collecting individual student details</li>
- *   <li>Printing the report card for each student</li>
+ * <li>Accepting the number of students</li>
+ * <li>Collecting individual student details</li>
+ * <li>Printing the report card for each student</li>
  * </ul>
  * </p>
  * 
@@ -19,36 +19,36 @@ import java.util.Scanner;
  */
 public class StudentApp {
 
-    /**
-     * The main method to run the student grading application.
-     * It collects data for multiple students and prints report cards.
-     *
-     * @param args command-line arguments (not used)
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	/**
+	 * The main method to run the student grading application. It collects data for
+	 * multiple students and prints report cards.
+	 *
+	 * @param args command-line arguments (not used)
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // Prompt user for number of students
-        System.out.println("Enter Number of Students");
-        int numStudents = scanner.nextInt();
+		// Prompt user for number of students
+		System.out.println("Enter Number of Students");
+		int numStudents = scanner.nextInt();
 
-        // Create an array to hold Student objects
-        Student students[] = new Student[numStudents];
+		// Create an array to hold Student objects
+		Student students[] = new Student[numStudents];
 
-        // Loop to collect details for each student
-        for (int i = 0; i < numStudents; i++) {
-            System.out.println("Enter Student Number " + (i + 1) + "'s Details");
-            students[i] = new Student();           // Instantiate student
-            students[i].inputDetails();            // Get student input
-        }
+		// Loop to collect details for each student
+		for (int i = 0; i < numStudents; i++) {
+			System.out.println("Enter Student Number " + (i + 1) + "'s Details");
+			students[i] = new Student(); // Instantiate student
+			students[i].inputDetails(); // Get student input
+		}
 
-        // Loop to print the report card for each student
-        for (Student student : students) {
-            student.printReportCard();
-        }
+		// Loop to print the report card for each student
+		for (Student student : students) {
+			student.printReportCard();
+		}
 
-        // Close the scanner resource
-        scanner.close();
-    }
+		// Close the scanner resource
+		scanner.close();
+	}
 
 }
